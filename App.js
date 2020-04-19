684a5100074f884b0a2ea00a6c4273e8ddd6cba2
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-04-19 19:08:09 +0100

    Add functionality to the timer

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -11,7 +11,93 @@ import {
 import TopBar from './components/TopBar';
 import Icon from 'react-native-vector-icons/Entypo';
 
+let timerMinutes = 25;
+
+let currentTime = timerMinutes * 60 * 1000;
+
 const App = () => {
+  const [countDown, setCountDown] = useState(countDown || null);
+
+  // const [currentTime, setCurrentTime] = useState(25 * 60 * 1000);
+  const [minutes, setMinutes] = useState(
+    (minutes || 25).toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+    }),
+  );
+  const [seconds, setSeconds] = useState(
+    (seconds || 0).toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+    }),
+  );
+
+  function updateTime(num, func) {
+    func(
+      num.toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+      }),
+    );
+  }
+
+  function startCountdown(time) {
+    const countDownTime = new Date().getTime() + time;
+    console.log(time, countDownTime);
+    if (countDown) {
+      clearInterval(countDown);
+    }
+
+    setCountDown(
+      setInterval(() => {
+        const now = new Date().getTime();
+
+        currentTime = countDownTime - now;
+
+        updateTime(
+          Math.floor((currentTime % (1000 * 60 * 60)) / (1000 * 60)),
+          setMinutes,
+        );
+        console.log(
+          (5).toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+          }),
+        );
+        updateTime(Math.floor((currentTime % (1000 * 60)) / 1000), setSeconds);
+
+        if (currentTime < 0) {
+          stopCountdown();
+          resetCountdown();
+        }
+      }, 500),
+    );
+  }
+
+  function stopCountdown() {
+    clearInterval(countDown);
+    setCountDown(null);
+  }
+
+  function resetCountdown() {
+    updateTime(0, setMinutes);
+    updateTime(0, setSeconds);
+    currentTime = timerMinutes * 60 * 1000;
+  }
+
+  function reset() {
+    stopCountdown();
+    resetCountdown();
+  }
+
+  function start() {
+    if (!countDown) {
+      console.log('from start', currentTime);
+      startCountdown(currentTime);
+    }
+  }
+
+  function stop() {
+    clearInterval(countDown);
+    setCountDown(null);
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -26,16 +112,20 @@ const App = () => {
                 <TopBar />
               </View>
               <View style={styles.displayContainer}>
-                <Text style={styles.display}>00:00</Text>
+                <Text style={styles.display}>
+                  <Text>{minutes}</Text>
+                  <Text>:</Text>
+                  <Text>{seconds}</Text>
+                </Text>
               </View>
               <View style={styles.controls}>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={start}>
                   <Icon name="controller-play" size={48} color="#274fed" />
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={stop}>
                   <Icon name="controller-stop" size={48} color="#274fed" />
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={reset}>
                   <Icon name="cycle" size={48} color="#274fed" />
                 </TouchableOpacity>
               </View>
